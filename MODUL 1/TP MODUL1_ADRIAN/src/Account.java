5ce0bfc4c27a704f0d29d8b0142dc4ba962676c3
adriandk <adriandaniel1803@gmail.com>
2023-10-20 18:17:57 +0700

    start working on TP modul 1

---
+++ MODUL 1/TP MODUL1_ADRIAN/src/Account.java
@@ -0,0 +1,16 @@
+public class Account {
+
+    private String Name;
+    private int accountNumber;
+    private int accountBalance;
+
+    public void deposit(){
+
+    }
+
+    public boolean withdraw(){
+
+        
+        return true;
+    }
+}
---
+++ MODUL 1/TP MODUL1_ADRIAN/src/Bank.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+public class Bank {
+
+    LinkedList<String> bankAccount = new LinkedList<String>();
+    
+    public void AddAccount() {
+
+    }
+
+    public void RemoveAccount() {
+
+    }
+    public void GetAccount() {
+
+    }
+    public void GetAllAccount() {
+
+    }
+
+}
